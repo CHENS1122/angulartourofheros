50543293e8f2265b403cd0a3d23da8cdf8c26277
ChenS <chen.conspec@gmail.com>
2019-07-13 22:10:46 +0800

    add lift-time-cycle test

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,21 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { HeroService } from '../hero.service';
 import { Hero } from '../hero';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent {
 
   heroes: Hero[];
 
-  constructor(private heroSvc: HeroService) {
+  constructor(private msgService:MessageService, private heroSvc: HeroService) {
+  
+  }
+
+  ngOnChanges() {
+      this.msgService.add('ngOnChanged')
   }
 
   ngOnInit() {
+    this.msgService.add('ngOnInit');
     this.heroSvc.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+
+  ngDoCheck() {
+    this.msgService.add('ngDoChecked');
+  }
+
+  ngAfterContentInit() {
+    this.msgService.add('ngOnAfterContentInit');
+  }
+
+  ngAfterContentChecked() {
+    this.msgService.add('ngAfterContentChecked');
+  }
+
+  ngAfterViewInit() {
+    this.msgService.add('ngAfterViewInit');
+  }
+
+  ngAfterViewChecked() {
+    this.msgService.add('ngAfterCngAfterViewCheched');
+  }
+
+  ngOnDestroy() {
+    this.msgService.add('ngOnDestroy');
+  }
+
+
+
+  ngOnViewInit() {
+    this.msgService.add('ogOnViewInit');
   }
 
   searchHeros(term: string) {
